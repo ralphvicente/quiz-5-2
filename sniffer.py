f6a34b4dbf6820b2a5f7129e24b921444c98ef16
ralphvicente <112498894+ralphvicente@users.noreply.github.com>
2022-12-01 21:57:55 -0600

    Add files via upload

--- sniffer.py
+++ sniffer.py
@@ -6,38 +6,36 @@ def main():
         print_menu()
         option : str = input('Choose a menu option: ')
         if option == '1':
-            number : int = int(input("Number of packets to be sent: "))
-            interval : int = int(input("Number of seconds between each packet: "))
+            number  = input("Number of packets to be sent: ")
+            interval = input("Number of seconds between each packet: ")
             print("Creating and sending packets ...")
             send_pkt(number, interval)
         elif option == '2':
             print("Listening to all traffic and show all ...")
-            capture = sniff(prn= lambda x: x.show(), iface='eth0')
+            sniff(iface='eth1', prn= lambda x: x.show())
         elif option == '3':
             print("Listening to ping command to the address 8.8.4.4 ...")
-            capture = sniff(prn= lambda x: print_pkt(x), filer='icmp and dst 8.8.4.4')
+            sniff(filter='icmp and dst 8.8.4.4' ,prn= lambda x: print_pkt(x))
         elif option == '4':
             print("Listening to telnet command executed from localhost ...")
-            capture = sniff(prn= lambda x: print_pkt(x), filter='tcp and src 127.0.0.1')
+            sniff(prn= lambda x: print_pkt(x), filter='tcp and src 127.0.0.1')
         elif option == '5':
             print("End")
             break
         else:
-            print(f"\nInvalid entry\n")
+            print("\nInvalid entry")
 
 
-"""
-@brief      Creates and sends multilayer packet
-@param      number: int     number of packets to be sent 
-@param      interval: int   the number of seconds between each packet
-"""
-def send_pkt(number:int, interval:int) -> None:
-    Ethernet_Layer : Ether = Ether(src='00:ae:f3:52:aa:d1', dst='00:02:15:37:a2:44')
-    IP_Layer : IP = IP(src='192.168.10.4', dst='192.168.6.12', ttl=32)
-    ICMP_Layer : ICMP = ICMP(type='echo-request')
-    Payload : str = "CSCE 313 secret message"
 
-    pkt : Packet = IP_Layer/ICMP_Layer/Payload
+def send_pkt(number, interval):
+    """Send a custom packet"""
+    #TODO
+
+    ipLayer = IP(src='192.168.10.4', dst='192.168.6.12', ttl=32)
+    icmpLayer = ICMP(type='echo-request')
+    payload : str = "CSCE 313 secret message"
+
+    pkt = ipLayer/icmpLayer/payload
 
     pkt.show()
 
